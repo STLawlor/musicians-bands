57b70d547d5d580b4eef361b95b750e2c351dc77
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-16 15:51:50 +0000

    add band2 in before all

--- index.test.js
+++ index.test.js
@@ -12,6 +12,12 @@ describe("Band and Musician Models", () => {
     await sequelize.sync({ force: true });
 
     newBand = await Band.create({
+      name: "Spice Ladies",
+      genre: "Pop",
+      showCount: 0,
+    });
+
+    newBand2 = await Band.create({
       name: "Spice Girls",
       genre: "Pop",
       showCount: 100,
@@ -30,20 +36,20 @@ describe("Band and Musician Models", () => {
 
   test("can create a Band", async () => {
     expect(newBand).toBeInstanceOf(Band);
-    expect(newBand.name).toBe("Spice Girls");
+    expect(newBand.name).toBe("Spice Ladies");
     expect(newBand.genre).toBe("Pop");
-    expect(newBand.showCount).toBe(100);
+    expect(newBand.showCount).toBe(0);
   });
 
   test("can update a Band", async () => {
-    await newBand.update({ showCount: 101 });
-    expect(newBand.showCount).toBe(101);
+    await newBand.update({ showCount: 1 });
+    expect(newBand.showCount).toBe(1);
   });
 
   test("can delete a Band", async () => {
     await newBand.destroy();
     const allBands = await Band.findAll();
-    expect(allBands).toEqual([]);
+    expect(allBands.length).toEqual(1);
   });
 
   test("can create a Musician", async () => {
@@ -53,12 +59,6 @@ describe("Band and Musician Models", () => {
   });
 
   test("Muscians associate with correct Band", async () => {
-    let newBand2 = await Band.create({
-      name: "Spice Girls",
-      genre: "Pop",
-      showCount: 100,
-    });
-
     await newBand2.addMusician(newMusician);
     await newBand2.addMusician(newMusician2);
     const bandMusicians = await newBand2.getMusicians();
